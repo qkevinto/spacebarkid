ea45731ac9f75a1dd29ab9fe2cb9e9c8301443af
Kevin To <hello@kevinto.me>
2015-11-13 22:05:19 +1100

    Clean up some eslint errors.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,30 +1,34 @@
 var transit = {
   out: function(elements, callback) {
+    'use strict';
+
     var i = 0;
     var tally = 0;
+    var onAnimationEnd = function() {
+      tally++;
+
+      if (tally === elements.length - 1) {
+        callback();
+      }
+    };
 
     for (i; i < elements.length; i++) {
       var transitInClass = elements[i].getAttribute('data-transit-in');
       var transitOutClass = elements[i].getAttribute('data-transit-out');
 
       elements[i].classList.remove(transitInClass);
-      elements[i].addEventListener('animationend', function() {
-        tally ++;
-
-        if (tally === elements.length - 1) {
-          callback();
-        }
-      });
+      elements[i].addEventListener('animationend', onAnimationEnd);
       elements[i].classList.add(transitOutClass);
     }
   }
-}
+};
 
 var transitElements = document.querySelectorAll('[data-transit-out]');
 var linkElements = document.querySelectorAll('a[href]');
 var linkElementsArray = [].slice.call(linkElements, 0);
 
 linkElementsArray.forEach(function(element) {
+  'use strict';
   var href = element.hasAttribute('href') ? element.getAttribute('href') : false;
   if (href) {
     element.addEventListener('click', function(event) {
