93fc13f1fe3928441f0571dd922da12ccdb19a2a
Kevin To <hello@kevinto.me>
2015-11-23 22:50:39 +1100

    Add support for error page.

---
+++ app/site/snippets/generic.php
@@ -0,0 +1,8 @@
+<article class="Piece Animation Animation-fadeIn" data-transit-in="Animation-fadeIn" data-transit-out="Animation-fadeOut">
+  <section class="Section">
+    <div class="Grid Grid--small">
+      <h1><?php echo $page->title(); ?></h1>
+      <?php echo $page->text()->kirbytext(); ?>
+    </div>
+  </section>
+</article>
--- app/site/templates/default.php
+++ app/site/templates/default.php
@@ -1,8 +1,7 @@
 <?php snippet('head') ?>
 <?php snippet('navigation') ?>
 <main class="main">
-  <?php snippet('hero') ?>
-  <?php snippet('pieces') ?>
+  <?php snippet('generic') ?>
   <?php snippet('about') ?>
 </main>
 <?php snippet('bottom') ?>
