a081f92f8cbd2bdba637e312a4089456d4c2cdb3
Kevin To <hello@kevinto.me>
2015-11-24 23:27:56 +1100

    Add favicons.

--- app/site/snippets/head.php
+++ app/site/snippets/head.php
@@ -6,6 +6,25 @@
     <meta name="keywords" content="<?php echo $site->keywords(); ?>">
     <meta name="author" content="<?php echo $site->author(); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png?v=m22aP0WveQ">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png?v=m22aP0WveQ">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png?v=m22aP0WveQ">
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png?v=m22aP0WveQ">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png?v=m22aP0WveQ">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png?v=m22aP0WveQ">
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png?v=m22aP0WveQ">
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png?v=m22aP0WveQ">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png?v=m22aP0WveQ">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png?v=m22aP0WveQ" sizes="32x32">
+    <link rel="icon" type="image/png" href="/android-chrome-192x192.png?v=m22aP0WveQ" sizes="192x192">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png?v=m22aP0WveQ" sizes="96x96">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png?v=m22aP0WveQ" sizes="16x16">
+    <link rel="manifest" href="/manifest.json?v=m22aP0WveQ">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=m22aP0WveQ" color="#000000">
+    <link rel="shortcut icon" href="/favicon.ico?v=m22aP0WveQ">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png?v=m22aP0WveQ">
+    <meta name="theme-color" content="#ffffff">
     <title><?php echo $site->title() ?></title>
 
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
